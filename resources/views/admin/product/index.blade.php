fc6eaf4c8be49a9cc85b8fefbd429aaacbfd74a6
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-03-26 22:15:07 +0430

    index blade file for administrating product

---
+++ resources/views/admin/product/index.blade.php
@@ -0,0 +1,52 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products List') }}
+        </h2>
+    </x-slot>
+
+    <div class="max-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-2xl mt-4 px-6 py-6 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <table class="table-auto w-full justify-center text-center">
+                <thead class="border ">
+                <tr>
+                    <th>Product Name</th>
+                    <th>Brand</th>
+                    <th>Inventory</th>
+                    <th>Operation</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                @forelse ($products as $product)
+                    <tr class="border">
+                        <td>
+                            {{ $product->name }}
+                        </td>
+                        <td>
+                            {{ $product->brand }}
+                        </td>
+                        <td>
+                            {{ $product->inventory }}
+                        </td>
+                        <td>
+                            <form action="{{route('admin.product.destroy',$product->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Delete /</button>
+                                <a href="{{ route('product.show', $product->id) }}">
+                                    {{ __('Show') }}
+                                </a>
+                            </form>
+                        </td>
+                    </tr>
+                @empty
+                    <p> You don not define any product yet!</p>
+                @endforelse
+                </tbody>
+            </table>
+        </div>
+        {{--                <script src="{{ asset('js/myFunctions.js') }}"></script>--}}
+
+    </div>
+</x-app-layout>
