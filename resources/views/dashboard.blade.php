00d66dc9ec638d08055590b87e3bb0d475b1ea9a
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-03-26 15:11:38 +0430

    working dashboard view blade file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <a href="{{ auth()->user()->isAdmin() ? route('admin.product.index') : route('product.index') }}">
+                        {{ __('Product list') }}
+                    </a>
+                </div>
+            </div>
+            @if(auth()->user()->isAdmin())
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <a href="{{ route('admin.product.create') }}">
+                            {{ __('Create New Product') }}
+                        </a>
+                    </div>
+                </div>
+            @endif
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ route('order.index') }}">
+                        {{ __('Order list') }}
+                    </a>
                 </div>
             </div>
         </div>
