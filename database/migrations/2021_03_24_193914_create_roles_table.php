173f936db07ffc0fa547993b669d3aca294a0996
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-03-26 22:15:52 +0430

    roles tables

---
+++ database/migrations/2021_03_24_193914_create_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            [
+                'name' => 'admin'
+            ],
+            [
+                'name' => 'customer'
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
