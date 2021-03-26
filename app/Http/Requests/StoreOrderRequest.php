cacf83ced45ebee2bc7ea195ad5e3e7c8ce19d84
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-03-26 22:18:14 +0430

    add custom form request for controllers

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'order_quantity' => 'numeric|gte:1',
+            'product_id' => 'bail|exists:products,id',
+        ];
+    }
+}
